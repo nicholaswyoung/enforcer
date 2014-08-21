efb07ee1e788982dc33b07e12511445dc4718072
Nicholas Young <nicholas@nicholaswyoung.com>
2014-08-21 10:08:01 -0500

    Tweak API.

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-exports.https = function (req, res, next) {
-  var https = req.secure;
+exports.https = function (options) {
+  options = options || {};
 
-  if (!https) {
-    https = ('https' === req.headers['x-forwarded-proto']);
-  }
+  return function (req, res, next) {
+    var https = req.secure;
 
-  if (https) return next();
+    if (!https) {
+      https = ('https' === req.headers['x-forwarded-proto']);
+    }
 
-  if ('GET' === req.method) {
-    return res.redirect(
-      301,
-      'https://' + req.header('host') + req.originalUrl
-    );
-  } else if ('HEAD' === req.method) {
-    return next();
-  } else {
-    return res.send(403, 'Please use HTTPS.');
-  }
+    if (https) return next();
+
+    if ('GET' === req.method) {
+      return res.redirect(
+        301,
+        'https://' + req.header('host') + req.originalUrl
+      );
+    } else if ('HEAD' === req.method) {
+      return next();
+    } else {
+      return res.send(403, 'Please use HTTPS.');
+    }
+  };
 };
 
-exports.http = function (req, res, next) {
-  var https = req.secure;
+exports.http = function (options) {
+  options = options || {};
 
-  if (!https) {
-    https = ('https' === req.headers['x-forwarded-proto']);
-  }
+  return function (req, res, next) {
+    var https = req.secure;
 
-  if (https) {
-    return res.redirect(
-      301,
-      'http://' + req.header('host') + req.originalUrl
-    );
-  } else { return next(); }
+    if (!https) {
+      https = ('https' === req.headers['x-forwarded-proto']);
+    }
+
+    if (https) {
+      return res.redirect(
+        301,
+        'http://' + req.header('host') + req.originalUrl
+      );
+    } else { return next(); }
+  };
 };
